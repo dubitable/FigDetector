6411ed41fc3ad5ea9cc6d2be4566944185532309
dubitable <pierrequereuil@gmail.com>
2021-07-12 14:25:40 -0700

    Added up-to-date files

---
+++ model.py
@@ -0,0 +1,61 @@
+#Main library
+import tensorflow as tf
+
+#Helper libraries
+import os, pickle, random
+import numpy as np
+import pandas as pd
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+directory = "fruits"
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  directory,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  directory,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+ 
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+model = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(29)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+  
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
+
+model.save_weights('model1')
